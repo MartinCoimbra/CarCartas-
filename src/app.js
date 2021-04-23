3a2e34f6835be4db5926f0dcb4d24a04255c53a9
MartinCoimbra <martin.coimbra.2017@gmail.com>
2021-04-23 10:57:25 +0000

    Ordenar array por numero FUNCIONA

--- src/app.js
+++ src/app.js
@@ -93,7 +93,7 @@ btnClick.addEventListener("click", function(e) {
 
       /* Guardamos los valores. */
 
-      los2Arr[z] = [iconos[numDelIconoInicial], simbolo[numIconoCentro]];
+      los2Arr[z] = [simbolo[numIconoCentro], iconos[numDelIconoInicial]];
       z++;
     }
   }
@@ -121,7 +121,7 @@ const selectSort = arr => {
   let min = 0;
   while (min < arr.length - 1) {
     for (let i = min + 1; i < arr.length - 1; i++) {
-      if (arr[min][1] > arr[i][1]) {
+      if (arr[min] > arr[i]) {
         let aux = arr[min];
         arr[min] = arr[i];
         arr[i] = aux;
